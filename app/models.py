3a6e2a422f50f6d9523136565d9c639abb36cc6c
Gavin Kariuki <gavin.kariuki@student.moringaschool.com>
2021-11-11 19:33:21 +0300

    add few properties to the database

--- app/models.py
+++ app/models.py
@@ -23,6 +23,26 @@ class User(UserMixin,db.Model):
     street = db.Column(db.String(80), nullable = True)
     house_number = db.Column(db.String(10), nullable = True)
 
+    @property
+    def password(self):
+        raise AttributeError('You can not access the password attribute')
+
+    @password.setter
+    def password(self,password):
+        self.pass_secure = generate_password_hash(password)
+
+    def verify_password(self,password):
+        return check_password_hash(self.pass_secure,password)
+
+    def save_user(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def delete_user(self):
+        db.session.delete(self)
+        db.session.commit()
+
+
     def __init__(self, name, email, password, created_at):
         self.name = name
         self.email = email
