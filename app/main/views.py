6eb1a37ded8de6965031e564f9dbb8f8ced8f115
Gavin Kariuki <gavin.kariuki@student.moringaschool.com>
2021-11-12 00:24:29 +0300

    create cart system functions

--- app/main/views.py
+++ app/main/views.py
@@ -1,7 +1,8 @@
-from flask import render_template, flash, redirect, url_for, abort, flash
+from flask import render_template, flash, redirect, url_for, abort, flash, session
+from secrets import token_hex
 from flask_login import current_user, login_required
-from ..models import User, Menu
-from app.auth.views import logout
+from ..models import User, Menu, Sell
+from app.auth.views import login, logout
 from .. import db,photos, bcrypt
 from . import main
 from .forms import LocationForm, UserForm
@@ -66,3 +67,71 @@ def user_profile():
 @main.route('/about')
 def about_us():
     return render_template('main/about.html')
+
+###########################################################
+#################CHECKOUT SYSTEM PART######################
+
+@main.route('/cart')
+def cart():
+    if current_user.is_anonymous:
+        return redirect(url_for('auth.login'))
+
+    elif 'cart' not in session:
+        session['cart'] = {}
+
+    prices = {k : Menu.query.filter_by(name=k).first().price for k in session['cart'].keys()}
+    return render_template('cart.html', cart=session['cart'], prices=prices)
+
+@main.route('/cart/<string:item>')
+@main.route('/cart/<string:item>/<int:quantity>')
+def add_item(item = '', quantity = 1):
+    if current_user.is_anonymous:
+        return redirect(url_for('auth.login'))
+
+    if item != '':
+        if 'cart' in session:
+            cart = session["cart"]
+            if item in cart.keys():
+                cart[item] += quantity
+            else:
+                cart[item] = quantity
+
+            session["cart"] = cart
+
+        else:
+            session["cart"] = {item : quantity}
+
+        flash('Item has been added successfully')
+        return redirect(url_for('cart.cart'))
+
+@main.route('/cart/remove/<string:item')
+@login_required
+def remove_item(item):
+    cart = session.get('cart')
+    if cart and item in cart.keys():
+        cart.pop(item)
+        session['cart'] = cart
+        return redirect(url_for('cart.cart'))
+
+    return abort(404)
+
+@main.route('/cart/buy')
+@login_required
+def buy_item():
+    prices = {k : Menu.query.filter_by(name=k).first().price for k in session['cart'].keys()}
+    return render_template('buy.html', current_user = current_user, cart = session['cart'], prices = prices)
+
+@main.route('/cart/success', methods = ['GET', 'POST'])
+@login_required
+def item_success():
+    while True:
+        sell_number = f'{current_user.street[:2]}-{token_hex(5)}'
+        if not Sell.query.filter_by(sell_number = sell_number).first():
+            user_id = current_user.id
+            final_price = sum([quantity * Menu.query.filter_by(name = product).first().price for product, quantity in session['cart'].items()])
+            sell = Sell(user_id, sell_number,final_price)
+            db.session.add(sell)
+            db.session.commit()
+            break
+
+        return render_template('success.html')
