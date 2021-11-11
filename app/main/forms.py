ddb8635f37e8dec8e292732afaf20e4625d29806
Gavin Kariuki <gavin.kariuki@student.moringaschool.com>
2021-11-11 22:54:17 +0300

    add forms for location and userform

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,4 +1,16 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SelectField, TextAreaField, SubmitField, ValidationError
+from wtforms.fields.simple import PasswordField
 from wtforms.validators import Required, Email, EqualTo
 
+class LocationForm(FlaskForm):
+    street = StringField('Name of your street', validators=[Required()])
+    house_number = StringField('Your house number', validators=[Required()])
+    phone_number = StringField('Your mobile number', validators=[Required()])
+    submit = SubmitField('Submit')
+
+class UserForm(FlaskForm):
+    username = StringField('Your username')
+    email = StringField('Your email address', validators=[Email()])
+    password = PasswordField('Enter your password')
+    submit = SubmitField('Submit')
